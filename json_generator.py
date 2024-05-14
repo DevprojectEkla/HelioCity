a014ed5a1222f92c322ff81e428c279a37245d8e
ekla <barlavigabriel@gmail.com>
2024-05-14 12:44:19 +0200

    json_generator almost done

--- json_generator.py
+++ json_generator.py
@@ -1,10 +1,11 @@
-from datetime import datetime
-import json
+import json, threading
+from multiprocessing import Process
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plot
-from connect_db import conn_alchemy_with_url, connect_with_alchemy, open_config
+from datetime import datetime
+from connect_db import conn_alchemy_with_url
 from database_handler import DatabaseHandler
-import numpy as np
 
 
 
@@ -28,11 +29,16 @@ class JSONGenerator(DatabaseHandler):
         self.flag_ts = False
         self.dates_column = None 
         self.pivot = None
-
+        self.plot_process = None
+    
+    def disconnect(self):
+        super().disconnect()
+        if hasattr(self,'plot_process') and isinstance(self.plot_process,Process) and self.plot_process.is_alive():
+            self.plot_process.terminate()
+    
     def init_generator(self, table_name, columns, graph_type):
         self.timestamp = datetime.now().strftime('%d-%m-%y')
-        self.json_filename = f"""./data/{self.timestamp}
-                            _{self.x_axis}-{self.y_axis}.json"""
+        self.json_filename = f"./data/{self.timestamp}_{self.x_axis}-{self.y_axis}.json"
         self.columns = columns
         if len(self.columns) >= 2:
             self.x_axis, self.y_axis = self.columns[:2]
@@ -65,8 +71,9 @@ class JSONGenerator(DatabaseHandler):
             self.df[self.dates_column] = pd.to_datetime(
                                             self.df[self.dates_column])
             self.df['day'] = self.df[self.dates_column].dt.date
-            self.df['time'] =self.df[self.dates_column].dt.time
-
+            self.df['time'] = self.df[self.dates_column].dt.time
+            self.df['hours'] = self.df[self.dates_column].dt.hour
+            
     def display_df(self):
         if self.df is not None:
             print(self.df,[self.df[col] for col in self.columns])
@@ -75,66 +82,120 @@ class JSONGenerator(DatabaseHandler):
 
 
     def filter_df(self,axis, min, max):
-
-        mask = (self.df[axis] >= min) & (self.df[axis] <= max)
-        self.df = self.df[mask]
-
-    def format_json(self):
-        # Creates the json data structure of key value pairs 
-        # based on column_names and values of self.columns
-        for column in self.columns:
-            self.json_format[column] = self.df[column].tolist()
-        self.json_format["type"] = self.graph_type
-
-    def _init_graph(self):
-        if self.graph_type == 'heat':
-            self._make_pivot()
-        else:
-            self.df.plot(x=self.x_axis,
-                         y=self.y_axis,
-                         kind='line',
-                         marker='o',
-                         linestyle='-')
         try:
-            self._create_labels()
+            mask = (self.df[axis] >= min) & (self.df[axis] <= max)
+            self.df = self.df[mask]
         except Exception as e:
-            print("Unable to create labels for our axis")
-
-    def plot_data(self):
-        self._init_graph()
-        plot.figure(figsize=(10, 6))
-        plot.imshow(self.pivot, cmap='hot_r',aspect='auto',
-                    interpolation='nearest')
-        plot.colorbar(label=z_axis)
-        plot.xlabel(x_axis)
-        plot.xticks(range(len(self.x_labels)), self.x_labels, rotation=0)
-        plot.yticks(range(len(self.y_labels)), self.y_labels, rotation=0)
-        plot.ylabel(y_axis)
-
-
+            print(f"cannot filter dataframe axis: {axis} with values min:{min}"
+                  f"and max:{max}\nerror:",e)
     def _make_pivot(self):
+        try:
     
             self.pivot = self.df.pivot_table(index=self.y_axis,
                                            columns=self.x_axis,
                                            values=self.z_axis,
                                            aggfunc='mean')
+        except Exception as e:
+            print("Error creating pivot",e)
         
-    def _create_labels(self):
+    def resample_times_to_hours(self):
+        #TODO
+            hourly_times = self.df.set_index('times').resample('H').sum().index
+
+    def _create_time_labels(self):
         try:
             dates = pd.to_datetime(self.pivot.columns)
 
-            self.x_labels = dates.strftime('%Y-%m-%d')
-            self.x_labels = [date for date in date_labels] 
-            self.y_labels = df['time']
-            unique_times = np.unique(df['time'].values)
-            self.y_labels = [time if i == 0 or time != unique_times[i-1]
-                             else '' for i, time in enumerate(unique_times)] 
+    # Filter times to display only hourly labels
+            hourly_times = self.df['hours']            
+            unique_times = np.unique(self.df['hours'].values)
+            times_labels = [time if i == 0 or time != unique_times[i-1] else '' for i, time in enumerate(unique_times)] 
 
+            # when there is too much data we don't want to overload the graph
+            # with labels on each ticks so we divide the number of labels while
+            # keeping data to be represented
+            # since there is as much 'days' value in the table 
+            # as there is 'time' we only need to take the common length n
+            dates = dates.strftime('%d-%m-%Y')
+            self.x_labels = [date for date in dates] 
+            # unique_times = np.unique(self.df['time'].values)
+            self.y_labels = times_labels
+            # self.y_labels = [time for time in hourly_times]
+            # self.y_labels = [time if i == 0 or time != unique_times[i-1]
+            #                  else '' for i, time in enumerate(unique_times)] 
         except Exception as e:
-            print("columns are not a timestamp format")
+            print("failed to create 'day' and 'time' labels\nerror:",e)
+            print("pivot parameter 'columns' are not a timestamp format\nerror:",e)
+    def _rearrange_columns(self):
+            # to plot a heatmap with days in abscissa and time in ordinate
+            # with plots of the quantity we want to represent in color
+            # the z axis is set to the value of y_axis (e.g temperature)
+            # and the x_axis 'date' is now split in:
+            # x_axis ='day' y_axis = 'time'
+            self.z_axis = self.y_axis
+            self.x_axis = 'day'
+            self.y_axis = 'time'
+
+    def plot_data(self):
+        if self.graph_type == 'heat':
+            try:
+                self._rearrange_columns()
+                self._make_pivot()
+                self._create_time_labels()
+                self.plot_heatmap()
+            except Exception as e:
+                print("Unable to create labels for our axis due to:",e)
+        else:
+           self.plot_scatter() 
+
+    def plot_scatter(self):
+        self.df.plot(x=self.x_axis,
+                         y=self.y_axis,
+                         kind='line',
+                         marker='o',
+                         linestyle='-')
+        plot.xlabel(self.x_axis)
+        plot.ylabel(self.y_axis)
+        plot.grid(True)
+
+        plot.show()
+
+    def plot_heatmap(self):
+        try:
+            plot.figure(figsize=(10, 6))
+        
+            plot.imshow(self.pivot, cmap='cool',aspect='auto',
+                    interpolation='nearest')
+            plot.colorbar(label=self.z_axis)
+            plot.xticks(range(len(self.x_labels)), self.x_labels, rotation=0)
+            plot.yticks(range(len(self.y_labels)), self.y_labels, rotation=0)
+            plot.xlabel(self.x_axis)
+            plot.ylabel(self.y_axis)
+            plot.show()
+        except Exception as e:
+            print("Unable to plot the heatmap due to:\nerror:",e)
+        # self.process = Process(target=plot.show)
+        # self.process.start()
+            # self.plot_thread = threading.Thread(target=plot.show)
+            # self.plot_thread.start()
+      
+    def format_json(self):
+        # Creates the json data structure of key value pairs 
+        # based on column_names and values of self.columns
+        data = {}
+        for key,column in zip(["x","y","z"],self.columns):
+            data[key] = self.df[column].tolist()
+        data["type"] = self.graph_type
+        self.json_format = json.dumps(data,
+                                      default=lambda x: str(x)
+                                      if isinstance(x,datetime)
+                                      else x,
+                                      indent=2
+                                      )
 
     def write_data_into_json(self):
         try:
+            self.format_json()
             with open(self.json_filename, 'w') as json_file:
 
                 json.dump(self.json_format, json_file)
@@ -145,16 +206,21 @@ class JSONGenerator(DatabaseHandler):
 
 
 if __name__ == '__main__':
+    # Test our class
 
     json_gen = JSONGenerator()
     json_gen.connect()
-    json_gen.init_generator('selected_date',['date','temperature'],'linear')
+    json_gen.init_generator('helio_step',['Date','Temperature'],'linear')
     json_gen.split_timestamps()
+    # json_gen.filter_df('Temperature',0,401)
     json_gen.display_df()
     input("continue?")
     json_gen.plot_data()
     input("continue?")
     json_gen.write_data_into_json()
+    json_gen.disconnect()
+
+    #boiler plates
     engine = conn_alchemy_with_url() 
     table_name = 'selected_date'
     x_axis = input(
@@ -239,16 +305,16 @@ if __name__ == '__main__':
 # timestamp = datetime.now().strftime('%d-%m-%y')
 # plot.savefig(f'./graph/{timestamp}_{x_axis}-{y_axis}.pdf')
 # plot.show()
-    plot.figure(figsize=(10, 6))
+    plot.figure(figsize=(100, 10))
     plot.imshow(pivot_df, cmap='hot_r',aspect='auto', interpolation='nearest')
     plot.colorbar(label=z_axis)
     plot.xlabel(x_axis)
     plot.xticks(range(len(date_labels)), date_labels, rotation=0)
     plot.yticks(range(len(times_labels)), times_labels, rotation=0)
-    plot.ylabel(y_axis)
-    # plot.show()
+    plot.ylabel(z_axis)
+    plot.show()
     heatmap_data = pivot_df.reset_index().to_dict(orient='records')
 
 # Convert data to JSON
-    heatmap_json = json.dumps(heatmap_data, indent=4)
-    print(heatmap_json)  # Print JSON data
+    # heatmap_json = json.dumps(heatmap_data, indent=4)
+    # print(heatmap_json)  # Print JSON data
